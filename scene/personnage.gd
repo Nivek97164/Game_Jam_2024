7a3dd5c6abf434e7c2f150240fec688c77c48366
Nivek97164 <kevinbernard971@gmail.com>
2024-01-23 11:00:58 +0100

    Base_Projet_Game_jam_2024

---
+++ scene/personnage.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+const JUMP_VELOCITY = -450.0
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("Right")
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("Left")
+	else:
+		_animated_sprite.play("Default")
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
