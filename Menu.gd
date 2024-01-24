2e3d87b686710decf636c3d520054d694b121739
Nivek97164 <kevinbernard971@gmail.com>
2024-01-24 16:43:16 +0100

    CA AVANCE !!!

--- Menu.gd
+++ Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scene/Niveau1.tscn")
+	get_tree().change_scene_to_file("res://scene/menu_histoire_et_jouabilité.tscn")
 
 
 func _on_button_2_pressed():
